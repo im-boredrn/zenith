83f0700e5cf77312cd4dd75d31dc1161d10ac9e2
ImBored <oamlskcusebutuoy@gmail.com>
2026-08-11 22:37:51 -0500

    Fully cleaned out unused Ability Factory Methods

--- zenith/Core/ZenithSystems.cs
+++ zenith/Core/ZenithSystems.cs
@@ -23,16 +23,14 @@ using static zenith.Core.ZenithBehavior;
 namespace zenith.Core
 {
   
-        public class ZenithSystems
-        {
-        public static bool DebugMode => ZenithSettings.ZDebugMode;
+    public class ZenithSystems
+    {
 
         public ZenithData ZenithData { get; }
         public DomainManager DomainManager { get; }
             public ProgressionManager ProgressionManager { get; }
         public ZenithGui ZenithGui { get; }
         public BearSenseRenderer BearSenseRenderer { get; }
-        public DomainDetailsGUI DomainDetailsGUI { get; }
 
         public AssimilationCore AssimilationCore { get; }
         public CreatureAdaptations CreatureAdaptations { get; }
@@ -105,20 +103,16 @@ namespace zenith.Core
             eventsWired = true;
 
 
-            foreach (var domain in DomainManager.Domains.Values) // per domain instance wire domain maxed event
+            foreach (var domain in DomainManager.Domains.Values) 
             {
                 domain.DomainMaxed += ProgressionManager.HandleDomainMaxed;
                 domain.DomainMaxed += () =>
                 {
                     Logger.Log(Player,$"[EVENT] DomainMaxed ZenithGui UpdateStats Called...");
-                    ZenithGui?.UpdateStats(); //() INVOKES - immediately call the method once it reaches this line of code
-                    if (CanUsePassive(domain))
-                    {
-                        Logger.Log(Player, $"[EVENT] Stats Refreshed!");
-                    }
+                    ZenithGui?.UpdateStats();                
                 };
 
-                domain.OnTierUp += (d) => // Action <d> so the parameters contain the object
+                domain.OnTierUp += (d) => 
                 {
                     ZenithGui?.UpdateStats();
                     Logger.Log(Player, $"[EVENT] {domain} tier increased to {d.GetTier()}");
@@ -127,7 +121,8 @@ namespace zenith.Core
                 };
 
             }
-            // On stage up Update GUI and check if domains can get passives
+
+
             ProgressionManager.OnStageUp += () =>
             {
                 Logger.Log(Player, $"[EVENT] StageUp ZenithGui UpdateStats Called...");
@@ -139,7 +134,6 @@ namespace zenith.Core
 
                 }
 
-                // ZenithGui?.BonusGUI.UpdateBonusStats();
             };
 
             ProgressionManager.OnProgressionChanged += () =>
@@ -171,7 +165,6 @@ namespace zenith.Core
         {
             var player = entity as EntityPlayer;
             if (player == null) return;
-      //      Log($"[FLOW] OnServerTick Called");
 
          
 
@@ -179,34 +172,17 @@ namespace zenith.Core
             {
                 serverTickable.OnTick(player,dt);
             }
-             //Log($"[DATA] Current Side is {player.World.Side}");
         }
 
         public void OnClientTick(float dt)
         {
             var player = entity as EntityPlayer;
             if (player == null) return;
-            //      Log($"[FLOW] OnClientTick Called");
            
             foreach (var tickable in TickManager.ClientTick)
             {
                 tickable.OnTick(player,dt);
-            }
-           
-           // Log($"CLIENT COUNT: {CreatureAdaptations.ActiveAdaptations.Count}");
+            }        
         }
-
-        private bool CanUsePassive( IDomainInfo domain)
-        {
-            var passiveReq = DomainManager.Domains[domain.GetDomain()].GetMaxTier() / 2;
-
-            return ProgressionManager.GetStage() >= 2 && domain.GetTier() >= passiveReq;
-        }
-
-      
-      
-
-
-        
     }
 }
